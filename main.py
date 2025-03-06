9f64723fb00457106f43fd98af3743eb98593d14
juliacarreras <julia.carreras03@gmail.com>
2025-03-06 14:23:21 +0100

    Rearrange code properly and print report

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from stats import get_num_words
-from stats import count_characters
-from stats import sorted_character_counts
+from stats import get_num_words, count_characters_ocurrences, sort_characters_by_count
+
+
+with open ("books/frankenstein.txt") as f:
+    book_text = f.read()
+
+word_count = get_num_words(book_text)
+character_counts = count_characters_ocurrences(book_text)
+sorted_character_counts = sort_characters_by_count(character_counts)
+
+print(f'''
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {word_count} total words 
+--------- Character Count -------''')
+
+for character, count in sorted_character_counts.items():
+    if character.isalpha():
+        print(f"{character}: {count}")
+
+print("============= END ===============")
